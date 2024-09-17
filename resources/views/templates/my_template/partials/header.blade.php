9050d4c1be350414b446c241e99dcc8cf2c83b45
Yaroslav Slashev <slasev903@gmail.com>
2024-09-17 16:50:53 +0300

    secure_asset заменил asset

--- resources/views/templates/my_template/partials/header.blade.php
+++ resources/views/templates/my_template/partials/header.blade.php
@@ -5,27 +5,27 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>@yield('title')</title>
-<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('templates/my_template/assets/images/favicons/apple-icon-57x57.png') }}">
-<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('templates/my_template/assets/images/favicons/apple-icon-60x60.png') }}">
-<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('templates/my_template/assets/images/favicons/apple-icon-72x72.png') }}">
-<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('templates/my_template/assets/images/favicons/apple-icon-76x76.png') }}">
-<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('templates/my_template/assets/images/favicons/apple-icon-114x114.png') }}">
-<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('templates/my_template/assets/images/favicons/apple-icon-120x120.png') }}">
-<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('templates/my_template/assets/images/favicons/apple-icon-144x144.png') }}">
-<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('templates/my_template/assets/images/favicons/apple-icon-152x152.png') }}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('templates/my_template/assets/images/favicons/apple-icon-180x180.png') }}">
-<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('templates/my_template/assets/images/favicons/android-icon-192x192.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('templates/my_template/assets/images/favicons/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('templates/my_template/assets/images/favicons/favicon-96x96.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('templates/my_template/assets/images/favicons/favicon-16x16.png') }}">
+<link rel="apple-touch-icon" sizes="57x57" href="{{ secure_asset('templates/my_template/assets/images/favicons/apple-icon-57x57.png') }}">
+<link rel="apple-touch-icon" sizes="60x60" href="{{ secure_asset('templates/my_template/assets/images/favicons/apple-icon-60x60.png') }}">
+<link rel="apple-touch-icon" sizes="72x72" href="{{ secure_asset('templates/my_template/assets/images/favicons/apple-icon-72x72.png') }}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{ secure_asset('templates/my_template/assets/images/favicons/apple-icon-76x76.png') }}">
+<link rel="apple-touch-icon" sizes="114x114" href="{{ secure_asset('templates/my_template/assets/images/favicons/apple-icon-114x114.png') }}">
+<link rel="apple-touch-icon" sizes="120x120" href="{{ secure_asset('templates/my_template/assets/images/favicons/apple-icon-120x120.png') }}">
+<link rel="apple-touch-icon" sizes="144x144" href="{{ secure_asset('templates/my_template/assets/images/favicons/apple-icon-144x144.png') }}">
+<link rel="apple-touch-icon" sizes="152x152" href="{{ secure_asset('templates/my_template/assets/images/favicons/apple-icon-152x152.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('templates/my_template/assets/images/favicons/apple-icon-180x180.png') }}">
+<link rel="icon" type="image/png" sizes="192x192"  href="{{ secure_asset('templates/my_template/assets/images/favicons/android-icon-192x192.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ secure_asset('templates/my_template/assets/images/favicons/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="96x96" href="{{ secure_asset('templates/my_template/assets/images/favicons/favicon-96x96.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('templates/my_template/assets/images/favicons/favicon-16x16.png') }}">
 <link rel="manifest" href="/manifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="assets/images/favicons/ms-icon-144x144.png">
 <meta name="theme-color" content="#ffffff">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<link rel="stylesheet" href="{{ asset('templates/my_template/assets/css/bootstrap.min.css') }}"/>
-<link rel="stylesheet" href="{{ asset('templates/my_template/assets/css/style.css') }}"/>
-<link rel="stylesheet" href="{{ asset('templates/my_template/assets/css/mobile.css') }}"/>
+<link rel="stylesheet" href="{{ secure_asset('templates/my_template/assets/css/bootstrap.min.css') }}"/>
+<link rel="stylesheet" href="{{ secure_asset('templates/my_template/assets/css/style.css') }}"/>
+<link rel="stylesheet" href="{{ secure_asset('templates/my_template/assets/css/mobile.css') }}"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"
     integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw=="
     crossorigin="anonymous" referrerpolicy="no-referrer"/>
