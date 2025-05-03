0c78225254ba8d3b35c38feb38892d059c83839b
Yalasev903 <slasev903@gmail.com>
2025-05-03 09:56:52 +0300

    Force migrate for production

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,14 +5,20 @@ cd /var/www
 # Копируем .env, если отсутствует
 [ ! -f .env ] && cp .env.example .env
 
-# Генерируем APP_KEY, если не установлен
+# Генерация ключа, если пустой
 if ! grep -q "^APP_KEY=" .env || [ -z "$(grep ^APP_KEY= .env | cut -d '=' -f2)" ]; then
     php artisan key:generate
 fi
 
-# Миграции и кеш
+# Удалим предыдущие кэши
+php artisan config:clear || true
+php artisan cache:clear || true
+
+# Применим миграции с флагом --force
 php artisan migrate --force || true
+
+# Кэшируем конфигурацию
 php artisan config:cache || true
 
-# Старт php-fpm
+# Запускаем php-fpm
 php-fpm --nodaemonize --fpm-config /usr/local/etc/php-fpm.conf
