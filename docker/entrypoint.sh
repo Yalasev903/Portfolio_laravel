902aaaa71d80a80f981c1162cb9e450f069e7cc1
Yalasev903 <slasev903@gmail.com>
2025-05-03 09:58:43 +0300

    Fix: add --force to migration in prod

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,20 +5,20 @@ cd /var/www
 # Копируем .env, если отсутствует
 [ ! -f .env ] && cp .env.example .env
 
-# Генерация ключа, если пустой
+# Генерация APP_KEY, если не задан
 if ! grep -q "^APP_KEY=" .env || [ -z "$(grep ^APP_KEY= .env | cut -d '=' -f2)" ]; then
     php artisan key:generate
 fi
 
-# Удалим предыдущие кэши
+# Очистка и кеш
 php artisan config:clear || true
 php artisan cache:clear || true
 
-# Применим миграции с флагом --force
+# ВАЖНО: миграции с флагом --force
 php artisan migrate --force || true
 
-# Кэшируем конфигурацию
+# Конфигурация кеш
 php artisan config:cache || true
 
-# Запускаем php-fpm
+# Запуск php-fpm
 php-fpm --nodaemonize --fpm-config /usr/local/etc/php-fpm.conf
